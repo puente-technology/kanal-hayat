f89509bd3fd5fe76f5248d48b70537040b399c30
orderr66 <kananhagverdiyev@gmail.com>
2019-08-08 01:14:37 +0300

    lint fix

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Logo from './Logo'
 import './ContactUs.css'
 
 const logo = require('../../static/images/blackLogo.svg');
 
 
 const ContactUsPage = (props) => {
-  const { header, address, telefon, email } = props;
+  const {
+    header,
+    address,
+    telefon,
+    email,
+  } = props;
   return (
     <div className="contact-us">
       <div className="contactInfo">
         <div className="logoContainer">
           <div className="logo">
-            <img src={logo} alt="logo"/>
+            <img src={logo} alt="logo" />
           </div>
         </div>
-        <div className="ContactUsHeaderText" style={{ marginTop:'25px' }}>
+        <div className="ContactUsHeaderText" style={{ marginTop: '25px' }}>
           <p className="ContactUsHeaderTextStyle">
             {header}
           </p>
         </div>
-        <div className="ContactUsInfoText" style={{ marginTop:'11px' }}>
+        <div className="ContactUsInfoText" style={{ marginTop: '11px' }}>
           <p className="ContactUsInfoTextStyleBold">
-            Telefon: 
+            Telefon:
           </p>
           <p className="ContactUsInfoTextStyle">
             {telefon}
           </p>
         </div>
-        <div className="ContactUsInfoText" style={{ marginTop:'14px' }}>
+        <div className="ContactUsInfoText" style={{ marginTop: '14px' }}>
           <p className="ContactUsInfoTextStyleBold">
-            Email: 
+            Email:
           </p>
           <p className="ContactUsInfoTextStyle">
             {email}
@@ -39,7 +43,7 @@ const ContactUsPage = (props) => {
         </div>
         <div className="ContactUsInfoText" style={{ width: '265px', marginTop: '14px' }}>
           <p className="ContactUsInfoTextStyleBold">
-            Address: 
+            Address:
           </p>
           <p className="ContactUsInfoTextStyle">
             {address}
@@ -51,33 +55,33 @@ const ContactUsPage = (props) => {
           <p className="formHeader">
             İletişim Formu
           </p>
-          <div className="field" style={{marginTop: '25px'}}>
-              <div >
-                <input className="fieldStyle" type="text" name="name" placeholder= 'İsim-Soyisim *' />
-              </div>
-              <div style={{paddingLeft: '30px'}}>
-                <input className="fieldStyle" type="email" name="email" placeholder='E-mail *' />
-              </div>
+          <div className="field" style={{ marginTop: '25px' }}>
+            <div>
+              <input className="fieldStyle" type="text" name="name" placeholder="İsim-Soyisim *" />
+            </div>
+            <div style={{ paddingLeft: '30px' }}>
+              <input className="fieldStyle" type="email" name="email" placeholder="E-mail *" />
+            </div>
           </div>
-          <div className="field" style={{marginTop: '21px'}}>
-              <div>
-                <input className="fieldStyle" type="text" name="konu" placeholder='Konu *' />
-              </div>
-              <div style={{paddingLeft: '30px'}}>
-                <input className="fieldStyle" type="text" name="telefon" placeholder='Telefon *'/>
-              </div>
+          <div className="field" style={{ marginTop: '21px' }}>
+            <div>
+              <input className="fieldStyle" type="text" name="konu" placeholder="Konu *" />
+            </div>
+            <div style={{ paddingLeft: '30px' }}>
+              <input className="fieldStyle" type="text" name="telefon" placeholder="Telefon *" />
+            </div>
           </div>
-              <div className="textArea" style={{marginTop: '21px'}}>
-                <p className="messageLabel">
-                  Mesaj *
-                </p>
-                <hr className="lineStyle" />
-                  <textarea name="message" className="textAreaStyle"></textarea>
-                <hr className="lineStyle" />
-              </div>
-            <p style={{ textAlign: 'center' }}>
-              <button className="buttonStyle" type="submit">Gönder</button>
+          <div className="textArea" style={{ marginTop: '21px' }}>
+            <p className="messageLabel">
+              Mesaj *
             </p>
+            <hr className="lineStyle" />
+            <textarea name="message" className="textAreaStyle" />
+            <hr className="lineStyle" />
+          </div>
+          <p style={{ textAlign: 'center' }}>
+            <button className="buttonStyle" type="submit">Gönder</button>
+          </p>
         </form>
       </div>
     </div>
