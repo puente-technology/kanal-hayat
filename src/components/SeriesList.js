ff23849983cd6c620d4a50e4af5316688f955ada
sercanatx <sercan@atxanka.io>
2019-08-26 20:28:36 +0300

    series list

---
+++ src/components/SeriesList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+
+const SeriesList = (props) => {
+  console.log({ props });
+  return (
+    <div>
+      Hey
+    </div>
+  )
+}
+
+// SeriesList.propTypes = {
+// }
+
+export default SeriesList
---
+++ src/templates/SeriesList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import PropTypes from 'prop-types'
+import LayoutComp from '../components/LayoutComp'
+import SeriesList from '../components/SeriesList';
+
+// Export Template for use in CMS preview
+// export const AboutUsPageTemplate = data => (
+//   <AboutUs {...data} />
+// )
+
+// Export Default HomePage for front-end
+const SeriesListPage = ({ data: { page } }) => (
+  <LayoutComp>
+    <SeriesList {...page} {...page.frontmatter} />
+  </LayoutComp>
+)
+
+SeriesListPage.propTypes = {
+  data: PropTypes.any,
+}
+
+export default SeriesListPage;
+
+export const pageQuery = graphql`
+query SeriesList($id: String!, $locale: String) {
+  page: markdownRemark(id: { eq: $id }, frontmatter: { locale: { eq: $locale }}) {
+    html
+  }
+}
+`
