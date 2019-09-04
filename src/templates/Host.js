606f3505a0e700fa89af25ffcdb501259199ce33
sercanatx <sercan@atxanka.io>
2019-09-04 17:59:27 +0300

    host page has been added

--- src/templates/Host.js
+++ src/templates/Host.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-// import PropTypes from 'prop-types'
+import { graphql, Link } from 'gatsby'
+import Nav from '../components/Nav';
+import Content from '../components/Content';
+
+import './Host.scss'
 
 // Export Template for use in CMS preview
 // export const AboutUsPageTemplate = data => (
@@ -9,11 +12,63 @@ import { graphql } from 'gatsby'
 
 // Export Default HomePage for front-end
 const Host = (data) => {
-  console.log({ adfsfsfsdf: data });
+  const hostArr = data.data.host.nodes
+  const { frontmatter } = hostArr[0]
+  const hostName = frontmatter.host
+  const seriesArr = data.data.series.nodes
+  const series = seriesArr.filter(x => x.frontmatter.host === hostName)
+
   return (
-    <div>
-      HEY
-    </div>
+    <React.Fragment>
+      <div
+        style={{ background: `url(${frontmatter.coverImage})` }}
+        className="HostContainer"
+      >
+        <Nav color="light" />
+      </div>
+      <div className="HostDescription">
+        <Content source={frontmatter.html} />
+      </div>
+      <div className="HostSeries">
+        <span className="HostTile">
+          PROGRAMLAR
+          <img alt="Programlar" src="/images/Polygondown.png" />
+        </span>
+        <div className="HostSeries">
+          {
+            series.map((serie, key) => (
+              <div
+                type="button"
+                key={key}
+                style={{
+                  background: `url(${serie.frontmatter.coverImage})`,
+                }}
+                className="SerieCard"
+
+              >
+                <Link to={serie.fields.slug}>
+                  <button
+                    type="button"
+                    value={frontmatter.title}
+                    className="paravan"
+                  />
+                </Link>
+
+                <div className="SpanContainer">
+                  <span className="SerieCardTitle">
+                    {serie.frontmatter.title}
+                  </span>
+                  <span className="SerieCardHost">
+                    {serie.frontmatter.host}
+                  </span>
+                </div>
+              </div>
+
+            ))
+          }
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 
@@ -25,7 +80,7 @@ export default Host;
 
 export const pageQuery = graphql`
 query Host($slug: String!) {
-  allMarkdownRemark(filter: { fields: { slug: { eq: $slug } }}) {
+  host: allMarkdownRemark(filter: { fields: { slug: { eq: $slug } }}) {
     nodes {
       frontmatter {
         host
@@ -39,6 +94,20 @@ query Host($slug: String!) {
           guests
         }
       }
+    }
+  }
+
+  series: allMarkdownRemark(filter: {fields: {contentType: {regex: "/series/"}}}) {
+    nodes {
+      frontmatter {
+          host
+          publishDate
+          title
+          coverImage
+      }
+      fields {
+          slug
+        }
     }
   }
 }
