351966b34bb3b1744be68db83c7598b0cbf1265e
orderr66 <kananhagverdiyev@gmail.com>
2019-09-16 14:26:53 +0300

    home page carousel

---
+++ src/components/HomePageCarousel.js
@@ -0,0 +1,235 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { toggleDarkMode } from '../state/app';
+import './HomePageCarousel.scss';
+
+const arrovSvg = require('../../static/images/expand.svg');
+
+class Carousel extends Component {
+  static propTypes = {
+    episodes: PropTypes.any.isRequired,
+    activeEpisode: PropTypes.any.isRequired,
+    frontmatter: PropTypes.any.isRequired,
+    handleVideoUrlChange: PropTypes.any.isRequired,
+    durations: PropTypes.any,
+    hosts: PropTypes.any,
+    dispatch: PropTypes.any,
+  };
+
+  constructor(props) {
+    super(props)
+    const { episodes, activeEpisode } = this.props
+    this.state = {
+      items: episodes,
+      active: activeEpisode,
+      direction: '',
+    }
+    this.rightClick = this.moveRight.bind(this)
+    this.leftClick = this.moveLeft.bind(this)
+  }
+
+  generateItems() {
+    const {
+      frontmatter,
+      handleVideoUrlChange,
+      durations,
+      hosts,
+      dispatch,
+    } = this.props
+    const { active, items } = this.state
+    const itemsArr = []
+    let level
+    for (let i = active - 1; i < active + 1; i += 1) {
+      let index = i
+      if (i < 0) {
+        index = items.length + i
+      } else if (i >= items.length) {
+        index = i % items.length
+      }
+      level = active - i
+      itemsArr.push(<Item
+        key={index}
+        id={items[index]}
+        level={level}
+        frontmatter={frontmatter}
+        activeEpisode={active}
+        episodes={items}
+        handleVideoUrlChange={handleVideoUrlChange}
+        durations={durations}
+        hosts={hosts}
+        dispatch={dispatch}
+      />)
+    }
+    return itemsArr
+  }
+
+  moveLeft() {
+    const { active, items } = this.state
+    let newActive = active
+    newActive -= 1
+    setTimeout(() => {
+      this.setState({
+        active: newActive < 0 ? items.length - 1 : newActive,
+        direction: 'left',
+      })
+    }, 600)
+  }
+
+  moveRight() {
+    const { active, items } = this.state
+    const newActive = active
+    setTimeout(() => {
+      this.setState({
+        active: (newActive + 1) % items.length,
+        direction: 'right',
+      })
+    }, 600)
+  }
+
+  render() {
+    const { direction } = this.state
+    return (
+      <div id="carousel" className="noselect">
+        <button type="button" className="arrow arrow-left" onClick={this.leftClick}><img src={arrovSvg} alt="LeftArrow" /></button>
+        <div style={{ display: 'flex' }}>
+          <ReactCSSTransitionGroup
+            component={React.Fragment}
+            transitionName={direction}
+          >
+            {this.generateItems()}
+          </ReactCSSTransitionGroup>
+        </div>
+        <button type="button" className="arrow arrow-right" onClick={this.rightClick}><img src={arrovSvg} alt="RightArrow" /></button>
+      </div>
+    )
+  }
+}
+
+class Item extends React.Component {
+  static propTypes = {
+    level: PropTypes.any.isRequired,
+    id: PropTypes.any.isRequired,
+    episodes: PropTypes.any,
+    handleVideoUrlChange: PropTypes.any,
+    durations: PropTypes.any,
+    dispatch: PropTypes.any,
+    hosts: PropTypes.any,
+  };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  hanndlePlayClick = (e) => {
+    console.log('clicked')
+    console.log(e)
+    const { dispatch, durations, hosts } = this.props;
+    const { thumb, i } = JSON.parse(e.target.value)
+    dispatch(toggleDarkMode(
+      thumb,
+      [],
+      true,
+      i,
+      '',
+      this.handleCloseClick,
+      false,
+      durations,
+      false,
+      hosts,
+    ))
+  }
+
+  handleCloseClick = () => {
+  }
+
+
+  getDuration = (episode) => {
+    const { durations } = this.props
+    if (durations) {
+      const result = durations.map((el) => {
+        if (el[episode.youtubeURL.id]) {
+          return el[episode.youtubeURL.id]
+        }
+      })
+      return result
+    }
+  }
+
+  handleClick = () => {
+    const {
+      id,
+      episodes,
+      handleVideoUrlChange,
+    } = this.props
+    handleVideoUrlChange(id, episodes)
+  }
+
+  render() {
+    const {
+      level,
+      id,
+    } = this.props
+    console.log(id)
+    const className = `item homeLevel${level}`
+    return (
+      <div className={className}>
+        {
+        id.image
+          ? (
+            <button
+              value={JSON.stringify({ id, level })}
+              type="button"
+              onClick={this.hanndlePlayClick}
+              style={{
+                background: `url(${id.youtubeURL.imageURL}) no-repeat`,
+                position: 'relative',
+                backgroundSize: 'cover',
+                width: '280px',
+                height: '150px',
+              }}
+              src={id.image}
+              width={280}
+              height={150}
+              className="EpisodeVideo"
+            >
+              <div className="playParavan">
+                {this.getDuration(id)}
+              </div>
+            </button>
+          )
+          : (
+            <button
+              value={JSON.stringify({ id, level })}
+              onClick={this.hanndlePlayClick}
+              type="button"
+              style={{
+                background: `url(${id.youtubeURL.imageURL}) 50%`,
+                backgroundSize: 'cover',
+                position: 'relative',
+                width: '280px',
+                height: '150px',
+              }}
+              className="EpisodeVideo"
+            >
+              <div className="playParavan">
+                {this.getDuration(id)}
+              </div>
+            </button>
+          )
+      }
+      </div>
+    )
+  }
+}
+
+export default connect(state => ({
+  test: state,
+  durations: state.app.durations,
+  hosts: state.app.hosts,
+}), null)(Carousel)
--- src/components/ProgrammeHomePage.js
+++ src/components/ProgrammeHomePage.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import { toggleDarkMode } from '../state/app';
+import Carousel from './HomePageCarousel';
 import './ProgrammeHomePage.scss';
 
 class ProgrammeHomePage extends Component {
@@ -11,6 +12,7 @@ class ProgrammeHomePage extends Component {
     data: PropTypes.any,
     dispatch: PropTypes.any,
     durations: PropTypes.any,
+    hosts: PropTypes.any,
   };
 
   constructor(props) {
@@ -32,7 +34,7 @@ class ProgrammeHomePage extends Component {
   }
 
   hanndlePlayClick = (e) => {
-    const { dispatch, durations } = this.props;
+    const { dispatch, durations, hosts } = this.props;
     const { thumb, i } = JSON.parse(e.target.value)
     this.setState({ isOpen: true })
     dispatch(toggleDarkMode(
@@ -45,6 +47,7 @@ class ProgrammeHomePage extends Component {
       false,
       durations,
       false,
+      hosts,
     ))
   }
 
@@ -75,41 +78,66 @@ class ProgrammeHomePage extends Component {
           </div>
           <div className="ProgrammeThumbnails">
             {
-              tempData.thumbnails.map((thumb, i) => (
-                <div key={i} className="ProgrammeThumbnail">
+              tempData.thumbnails.length > 2 ? (
+                <Carousel
+                  episodes={tempData.thumbnails}
+                  activeEpisode={0}
+                />
+              ) : (
+                <div style={{ display: 'flex' }}>
                   {
-                    thumb.image
-                      ? (
-                        <img
-                          alt={`thumbnail${i}`}
-                          src={thumb.image}
-                          width={280}
-                          height={150}
-                        />
-                      )
-                      : (
-                        <button
-                          value={JSON.stringify({ thumb, i })}
-                          onClick={this.hanndlePlayClick}
-                          type="button"
-                          style={{
-                            background: `url(${thumb.youtubeURL.imageURL}) 50%`,
-                            backgroundSize: 'cover',
-                            position: 'relative',
-                            width: '280px',
-                            height: '150px',
-                          }}
-                          className="EpisodeVideo"
-                        >
-                          <div className="playParavan">
-                            {this.getDuration(thumb)}
-                          </div>
-                        </button>
-                      )
+                    tempData.thumbnails.map((thumb, i) => (
+                      <div key={i} className="ProgrammeThumbnail">
+                        {
+                          thumb.image
+                            ? (
+                              <button
+                                value={JSON.stringify({ thumb, i })}
+                                type="button"
+                                onClick={this.hanndlePlayClick}
+                                style={{
+                                  background: `url(${thumb.youtubeURL.imageURL}) no-repeat`,
+                                  position: 'relative',
+                                  backgroundSize: 'cover',
+                                  width: '280px',
+                                  height: '150px',
+                                }}
+                                src={thumb.image}
+                                width={280}
+                                height={150}
+                                className="EpisodeVideo"
+                              >
+                                <div className="playParavan">
+                                  {this.getDuration(thumb)}
+                                </div>
+                              </button>
+                            )
+                            : (
+                              <button
+                                value={JSON.stringify({ thumb, i })}
+                                onClick={this.hanndlePlayClick}
+                                type="button"
+                                style={{
+                                  background: `url(${thumb.youtubeURL.imageURL}) 50%`,
+                                  backgroundSize: 'cover',
+                                  position: 'relative',
+                                  width: '280px',
+                                  height: '150px',
+                                }}
+                                className="EpisodeVideo"
+                              >
+                                <div className="playParavan">
+                                  {this.getDuration(thumb)}
+                                </div>
+                              </button>
+                            )
+                        }
+                        {isOpen}
+                      </div>
+                    ))
                   }
-                  {isOpen}
                 </div>
-              ))
+              )
             }
           </div>
         </div>
