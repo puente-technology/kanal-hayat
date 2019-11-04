942750c98daf0fc6ce46b75127cf368af12e11e9
MAC <mac@MACs-MacBook-Pro.local>
2019-11-04 14:35:25 +0300

    padding top increased

--- src/components/SeriesPage.js
+++ src/components/SeriesPage.js
@@ -52,7 +52,7 @@ class SeriesPage extends Component {
     const { episodes, divHeight } = this.props;
     const { isOpen } = this.state;
     return (
-      <div className="SeriesPage" style={{ paddingTop: 400 }}>
+      <div className="SeriesPage" style={{ paddingTop: 430 }}>
         {
           episodes.map((episode, index) => (
             <div key={index} className="Episode">
