ec7299ee06aa1fe8af12ef34f434af8c1bd6cfc6
orderr66 <kananhagverdiyev@gmail.com>
2019-11-11 00:31:01 +0300

    new changes

--- src/components/MobileAppLink.js
+++ src/components/MobileAppLink.js
@@ -4,13 +4,13 @@ import './MobileAppLink.css';
 
 const googlePlay = require('../../static/images/googleplay.svg');
 const appStore = require('../../static/images/appstore.svg');
-const qrCode = require('../../static/images/qrcode.svg');
+const qrCode = require('../../static/images/qr.png');
 // const phone = require('../../static/images/telephone.svg');
 
 const MobileAppLink = () => (
   <div className="containerMobileAppLink">
     <div className="qrCodeStyle">
-      <img className="qrCode" alt="qrCode" key="qrCode" src={qrCode} />
+      <img className="qrCode" alt="qrCode" key="qrCode" height="200px" src={qrCode} />
     </div>
     <div className="appLink">
       <p className="firstText">
@@ -24,10 +24,10 @@ const MobileAppLink = () => (
         ut labore et dolore magna aliqua.
       </p>
       <div className="googlePlayStyle">
-        <a rel="noopener noreferrer" href="https://www.google.com" target="_blank">
+        <a rel="noopener noreferrer" href="https://play.google.com/store/apps/details?id=com.appmachine.p2697DD" target="_blank">
           <img alt="googleplay" key="googleplay" src={googlePlay} style={{ paddingRight: '24px' }} className="googlePlay" />
         </a>
-        <a rel="noopener noreferrer" href="https://www.google.com" target="_blank">
+        <a rel="noopener noreferrer" href="https://apps.apple.com/us/app/kanal-hayat/id869241958?amp%3Bamp%3Bmt=8&ls=1" target="_blank">
           <img alt="appstore" key="appstore" src={appStore} className="appStore" />
         </a>
       </div>
